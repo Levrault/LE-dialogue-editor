e62cb0a3b86cb0f42b8e0d2d3eac10613ca8b9f8
unknown <lucf.langis@gmail.com>
2020-08-01 19:07:35 +0200

    feat(json) add a choice

--- src/Autoload/Json.gd
+++ src/Autoload/Json.gd
@@ -29,12 +29,13 @@ func _ready() -> void:
 	Events.connect("choice_to_dialogue_relation_deleted", self, "_on_Choice_to_dialogue_relation_deleted")
 
 
-func get_template() -> Dictionary:
+func get_dialogue_template_string() -> Dictionary:
 	var template := {
 		"uuid": "[color=#ef5350]%uuid%[/color] {\n",
 		"name": "	[color=#2196f3]name[/color]: %name%,\n",
 		"portrait": "	[color=#2196f3]portrait[/color]: %portrait%,\n",
 		"text": "	[color=#2196f3]text[/color]: {\n",
+		"choices": "choices",
 		"en": "		en: %en%\n",
 		"fr": "		fr: %fr%\n",
 		"line": "	},\n",
@@ -43,6 +44,17 @@ func get_template() -> Dictionary:
 	}
 	return template
 
+
+func get_choice_template_string() -> Dictionary:
+	var template := {
+		"choices": "	[color=#03a9f4]choices[/color]:[\n",
+		"text": "		[color=#2196f3]text[/color]: {\n",
+		"en": "			en: %en%\n",
+		"fr": "			fr: %fr%\n",
+		"line": "		},\n",
+		"next": "		[color=#009688]next[/color]: %next%\n",
+	}
+	return template
 
 
 func _on_Dialogue_node_created(uuid: String) -> void:
--- src/Popup/RawJsonWindows.gd
+++ src/Popup/RawJsonWindows.gd
@@ -11,12 +11,30 @@ func _on_About_to_show() -> void:
 	json.bbcode_text = ""
 
 	for uuid in Json.json_raw:
-		var template: Dictionary = Json.get_template()
+		var template: Dictionary = Json.get_dialogue_template_string()
 		template.uuid = template.uuid.replace("%uuid%", uuid)
 		template.name = template.name.replace("%name%", Json.json_raw[uuid].name)
 		template.portrait = template.portrait.replace("%portrait%", Json.json_raw[uuid].portrait)
 		template.en = template.en.replace("%en%", Json.json_raw[uuid].text.en)
 		template.fr = template.fr.replace("%fr%", Json.json_raw[uuid].text.fr)
+
+		if Json.json_raw[uuid].has("choices"):
+			var choice_template = Json.get_dialogue_template_string()
+			var choices := ""
+			for choice in Json.json_raw[uuid].choices:
+				choice_template.text = choice_template.text.replace("text",  choice.text)
+				choice_template.en = choice_template.text.replace("en",  choice.en)
+				choice_template.fr = choice_template.text.replace("fr",  choice.fr)
+				choice_template.next = choice_template.text.replace("next",  choice.next)
+
+				for key in choice:
+					choices += choice[key]
+
+			template.choices = choices
+		else:
+			template.erase("choices")
+
+
 
 		if Json.json_raw[uuid].has("next"):
 			template.next = template.next.replace("%next%", Json.json_raw[uuid].next)
