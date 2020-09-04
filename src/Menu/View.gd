c7051f802de61b26fce7451e3ff28d9e69693653
levrault <lucf.langis@gmail.com>
2020-09-04 09:15:40 +0200

    feat(menu) add toggle icon

--- src/Menu/View.gd
+++ src/Menu/View.gd
@@ -1,6 +1,7 @@
 extends MenuButton
 
 enum Menu { preview, folder_view }
+var check_icon: Texture = preload("res://assets/icons/check.svg")
 
 
 func _ready():
@@ -12,10 +13,19 @@ func _ready():
 
 
 func _unhandled_key_input(event: InputEvent) -> void:
-	pass
+	if event.is_action_pressed("preview"):
+		Events.emit_signal("layout_preview_toggled")
+		_set_icon(Menu.preview)
+		return
+
+	if event.is_action_pressed("folder_view"):
+		Events.emit_signal("layout_folder_view_toggled")
+		_set_icon(Menu.folder_view)
+		return
 
 
 func _on_Item_pressed(id: int) -> void:
+	_set_icon(id)
 	if id == Menu.folder_view:
 		Events.emit_signal("layout_folder_view_toggled")
 		return
@@ -23,3 +33,10 @@ func _on_Item_pressed(id: int) -> void:
 	if id == Menu.preview:
 		Events.emit_signal("layout_preview_toggled")
 		return
+
+
+func _set_icon(id: int) -> void:
+	if get_popup().get_item_icon(id):
+		get_popup().set_item_icon(id, null)
+	else:
+		get_popup().set_item_icon(id, check_icon)
