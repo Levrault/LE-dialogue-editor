ac4339958fdfd94ff81fc9482143d83bbe4febcf
levrault <lucf.langis@gmail.com>
2020-09-17 14:01:01 +0200

    feat(preview) can now fold and unfold form

--- src/Preview/Form/TimelineForm.gd
+++ src/Preview/Form/TimelineForm.gd
@@ -2,6 +2,15 @@ extends VBoxContainer
 
 var conditions := {}
 onready var conditions_container := $ConditionsContainer
+onready var title := $Title
+
+
+func fold() -> void:
+	pass
+
+
+func unfold() -> void:
+	pass
 
 
 func _on_Condition_checked(name: String, value: bool) -> void:
@@ -9,3 +18,5 @@ func _on_Condition_checked(name: String, value: bool) -> void:
 		conditions.erase(name)
 		return
 	conditions[name] = true
+
+
