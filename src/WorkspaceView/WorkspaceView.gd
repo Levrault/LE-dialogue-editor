1f21423c236156b1e63ef35a10bfeda29fca2696
levrault <lucf.langis@gmail.com>
2020-11-24 13:47:14 +0100

    feat(workspace) add workspace name

--- src/WorkspaceView/WorkspaceView.gd
+++ src/WorkspaceView/WorkspaceView.gd
@@ -1,11 +1,13 @@
 extends Control
 
 onready var close_workspace_view := $MarginContainer/Wrapper/HBoxContainer/CloseWorkspaceView
+onready var label := $MarginContainer/Wrapper/HBoxContainer/Label
 
 func _ready() -> void:
 	Events.connect("layout_workspace_view_toggled", self, "_on_layout_workspace_view_toggled")
 	close_workspace_view.connect("pressed", Events, "emit_signal", ["layout_workspace_view_toggled"])
 	visible = Config.globals.views.workspace
+	label.text = Editor.workspace.name
 
 
 func _on_layout_workspace_view_toggled() -> void:
