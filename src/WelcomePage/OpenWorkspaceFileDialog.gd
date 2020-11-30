a0317f4af52f74822d50fde2ad8805ddfa8a0bfe
levrault <lucf.langis@gmail.com>
2020-11-30 14:09:02 +0100

    feat(welcome) prevent duplicate workspace to be added

--- src/WelcomePage/OpenWorkspaceFileDialog.gd
+++ src/WelcomePage/OpenWorkspaceFileDialog.gd
@@ -7,6 +7,16 @@ func _ready() -> void:
 
 
 func _on_File_selected(path: String) -> void:
+	for workspace in Config.globals.workspaces.list:
+		print(workspace)
+		if workspace.folder == path:
+			Events.emit_signal(
+				"notification_displayed",
+				Editor.Notification.error,
+				"%s already exist in the workspace" % path
+			)
+			return
+
 	Config.globals.workspaces.list.append(
 		{folder = path, name = current_file, resource = Config.get_workspace_resource(path)}
 	)
