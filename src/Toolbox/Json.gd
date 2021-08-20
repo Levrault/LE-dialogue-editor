9397317f94e4a09a101c0694a398eed35c8c667f
levrault <lucf.langis@gmail.com>
2021-08-20 15:30:30 -0400

    feat(json) add live update

--- src/Toolbox/Json.gd
+++ src/Toolbox/Json.gd
@@ -1,9 +1,15 @@
 extends RichTextLabel
 
+var values: Dictionary = {}
+
 
 func _ready():
 	Events.connect("toolbox_json_displayed", self, "_on_Json_displayed")
 
 
-func _on_Json_displayed(values: Dictionary) -> void:
+func _process(delta):
 	text = JSON.print(values, "\t")
+
+
+func _on_Json_displayed(node_values: Dictionary) -> void:
+	values = node_values
